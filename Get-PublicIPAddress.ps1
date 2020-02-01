4f0e380249d0a110478244bc30abdf323921c4ac
ThunderBall64 <33467576+ThunderBall64@users.noreply.github.com>
2020-02-01 16:46:06 -0500

    Update Get-PublicIPAddress.ps1

--- Get-PublicIPAddress.ps1
+++ Get-PublicIPAddress.ps1
@@ -1,33 +1,3 @@
-﻿<#
-.SYNOPSIS
-Get-PublicIPAddress.ps1 - Retrieve the public IP address of local or remote computer.
-.DESCRIPTION 
-This PowerShell script will retrieve the public IP address of the local computer, or
-a remote computer.
-.OUTPUTS
-Results are output to console.
-.PARAMETER Computer
-Specifies the remote computer that you want to connect to using PowerShell remoting
-and perform the public IP address query. If this parameter is not specified then
-the local computer is used.
-.EXAMPLE
-.\Get-PublicIPAddress.ps1
-Retrieves the public IP address for the local computer.
-.EXAMPLE
-.\Get-PublicIPAddress.ps1 -Computer SERVER01
-Retrieves the public IP address for the computer SERVER01.
-.NOTES
-Written by: Paul Cunningham
-Find me on:
-* My Blog:	https://paulcunningham.me
-* Twitter:	https://twitter.com/paulcunningham
-* LinkedIn:	https://au.linkedin.com/in/cunninghamp/
-* Github:	https://github.com/cunninghamp
-Change Log
-V1.00, 14/12/2015 - Initial version
-V1.01, 15/12/2015 - Changed to REST query against different API
-#>
-
 [CmdletBinding()]
 param (
 	
