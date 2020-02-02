8879b5260947a7e14bddea58e22bee292b80ad36
ThunderBall64 <33467576+ThunderBall64@users.noreply.github.com>
2020-02-01 19:31:27 -0500

    Install Adobe Reader Script
    I was tasked with building and deploying 130 workstations running Windows 10 Enterprise, is it possible?? Yes! It was, because I was able to automate several tasks that would consume time and delay production. Ever install Adobe Reader? Its not as easy as installing a browser, you have to be aware of which version you're installing and additional add-on features. When you download Adobe Reader it will automatically check "install Google Chrome/McAfee AV" which could wreak havoc and almost did when one of my technicians thought it'd be cool to install those while Adobe was installing. Since then, I created this script that will set execution policies to allow executable permissions with remote signatures. Upon completion, Adobe reader will be fully installed! :)

---
+++ Install-AdobeReader.ps1
@@ -0,0 +1,42 @@
+﻿#######################################################################################
+# Script    : Install_AdobeReader.ps1
+# Name      : Tyler Billings
+# Date      : 20 August 2019
+# Objective : Install Adobe Reader - used in non-profit agency Windows 10 Deployment. 
+#############################
+##### PowerShell Script #####
+#############################
+
+# Check execution policies
+$exePolicy = Get-ExecutionPolicy -List 
+$remSigned = Get-ExecutionPolicy RemoteSigned
+
+if ($exePolicy -eq $True) {
+    Write-Host "Set Execution Policies to run script"
+    Set-ExecutionPolicy RemoteSigned
+}
+
+$workdir = "c:\installer\"
+
+If (Test-Path -Path $workdir -PathType Container)
+{
+    Write-Host "$workdir already exists" -ForegroundColor Red
+}
+
+else {
+
+    New-Item -Path $workdir -ItemType Directory 
+}
+
+$source = "http://ardownload.adobe.com/pub/adobe/reader/win/AcrobatDC/1502320053/AcroRdrDC1502320053_en_US.exe"
+$destination = "$workdir\adobeDC.exe" 
+Invoke-WebRequest $source -OutFile $destination 
+
+# Begin installation
+Start-Process -FilePath "$workdir\adobeDC.exe" -ArgumentList "/sPB /rs"
+
+# Wait for installation to complete
+Start-Sleep -s 35 
+
+# Remove installer
+rm -Force $workdir\adobe*
